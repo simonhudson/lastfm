078f61d1200c2966296b3eeaf1660e3bb0194a9f
Simon Hudson <simon.hudson@experian.com>
2015-12-18 11:11:14 +0000

    Tidy up some redundant code

--- js/lastfm.js
+++ js/lastfm.js
@@ -32,7 +32,6 @@ var LastFM = {
     displayHeading: function(data) {
         $('.lastfm__avatar').attr('src', '');
         if (!data.error) {
-            // $('.lastfm__heading').before('<img class="lastfm__avatar" src="" />');
             $('.lastfm__heading').html(data.error ? data.message : '<a href="' + data.user.url + '">' + data.user.realname + '</a>');
             $('.lastfm__avatar').attr('src', data.user.image[2]['#text']);
         }
@@ -46,7 +45,7 @@ var LastFM = {
             for (var i in data.topartists.artist) {
                 var item = data.topartists.artist[i];
                 var imgUrl = item.image[2]['#text'] ? item.image[2]['#text'] : 'imgs/placeholder-note.svg';
-                listData += '<li class="lastfm-top-artists__item"><a class="lastfm-top-artists__link" href="' + item.url + '"><img class="img-circle lastfm-top-artists__img" src="' + imgUrl + '" /><div class="lastfm-top-artists__name img-circle"><p>' + item.name + '</p></div></a></li>';
+                listData += '<li class="lastfm-top-artists__item"><a class="lastfm-top-artists__link" href="' + item.url + '"><img class="img-circle lastfm-top-artists__img" src="' + imgUrl + '" /><div class="lastfm-top-artists__name img-circle"><p><strong>' + item.name + '</strong></p></div></a></li>';
             }
         }
         dataList.append(listData);
--- js/main.js
+++ js/main.js
@@ -3,20 +3,7 @@ $(document).ready(function() {
     $.when(LastFM.getUserInfo(user)).then(function(data) {
         LastFM.displayHeading(data);
         $.when(LastFM.getTopArtists(user, '12month')).then(function(data) {
-            console.log(data);
             LastFM.displayTopArtists(data);
         });
     });
 });
-
-// $('.lastfm-search__form').on('submit', function() {
-//     var user = $('#lastfm-search__user').val();
-//     // $.when(LastFM.getUserInfo(user)).then(function(data) {
-//     //     //LastFM.displayHeading(data);
-//     //     Url.updateUrlWithoutReload(data.user.name);
-//     // });
-//     $.when(LastFM.getRecentTracks(user)).then(function(data) {
-//         LastFM.displayRecentTracks(data);
-//     });
-//     return false;
-// });
